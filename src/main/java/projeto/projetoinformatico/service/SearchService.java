c66421a9bef138ee2d9d2ba1f7066c1fba04dab0
Tiago Pereira <tpereira1911@gmail.com>
2024-03-27 12:41:43 +0000

    Validation

--- src/main/java/projeto/projetoinformatico/service/SearchService.java
+++ src/main/java/projeto/projetoinformatico/service/SearchService.java
@@ -3,111 +3,114 @@ package projeto.projetoinformatico.service;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.RDFNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 import projeto.projetoinformatico.model.SearchResult;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+@EnableCaching
 @Service
 public class SearchService {
 
+    @Value("${sparql.endpoint}")
+    private String sparqlEndpoint; // Inject SPARQL endpoint URL
 
     @Cacheable(value = "searchCache", key = "{ #lat1, #lat2, #lon1, #lon2}")
-
-    public SearchResult performSearch(Double lat1, Double lon1, Double lat2, Double lon2)  {
-        String sparqlQuery = constructSparqlQuery(lat1, lon1,lat2,lon2);
-        List<Map<String, String>> results = executeSparqlQuery(sparqlQuery);
-        return new projeto.projetoinformatico.model.SearchResult(results); // Fully qualify the SearchResult class
+    public SearchResult performSearch(Double lat1, Double lon1, Double lat2, Double lon2) {
+        try {
+            // Validate the coordinates
+            validateCoordinates(lat1, lon1, lat2, lon2);
+            // Proceed with the search
+            String sparqlQuery = constructSparqlQuery(lat1, lon1, lat2, lon2);
+            List<Map<String, String>> results = executeSparqlQuery(sparqlQuery);
+            return new SearchResult(results);
+        } catch (IllegalArgumentException e) {
+            // Log the error
+            System.err.println("Invalid coordinates provided: " + e.getMessage());
+            // Return a response indicating the error
+            return new SearchResult(Collections.emptyList());
+        }
     }
-    @Cacheable(value = "searchCache", key = "{ #lat1, #lat2, #lon1, #lon2, #startTime, #endTime }")
 
-    public SearchResult performSearchTime(Double lat1, Double lon1, Double lat2, Double lon2, Long startTime, Long endTime)  {
-        String sparqlQuery = constructSparqlQueryTime(lat1, lon1,lat2,lon2 ,startTime, endTime);
-        List<Map<String, String>> results = executeSparqlQuery(sparqlQuery);
-        return new projeto.projetoinformatico.model.SearchResult(results);
+    @Cacheable(value = "searchCache", key = "{ #lat1, #lat2, #lon1, #lon2, #startTime, #endTime }")
+    public SearchResult performSearchTime(Double lat1, Double lon1, Double lat2, Double lon2, Long startTime, Long endTime) {
+        try {
+            // Validate the coordinates
+            validateCoordinates(lat1, lon1, lat2, lon2);
+
+            // Proceed with the search
+            String sparqlQuery = constructSparqlQueryTime(lat1, lon1, lat2, lon2, startTime, endTime);
+            List<Map<String, String>> results = executeSparqlQuery(sparqlQuery);
+            return new SearchResult(results);
+        } catch (IllegalArgumentException e) {
+            // Log the error
+            System.err.println("Invalid coordinates provided: " + e.getMessage());
+            // Return a response indicating the error
+            return new SearchResult(Collections.emptyList());
+        }
     }
 
     private List<Map<String, String>> executeSparqlQuery(String sparqlQuery) {
-        // Define the Wikidata SPARQL endpoint URL
-        String sparqlEndpoint = "https://query.wikidata.org/sparql";
-
-        // Create a Query instance
-        Query query = QueryFactory.create(sparqlQuery);
-
-        // Create a QueryExecution instance using the provided SPARQL endpoint
-        QueryExecution qexec = QueryExecutionFactory.sparqlService(sparqlEndpoint, query);
-
-        try {
-            // Execute the query and obtain the result set
+        try (QueryExecution qexec = QueryExecutionFactory.sparqlService(sparqlEndpoint, sparqlQuery)) {
             ResultSet results = qexec.execSelect();
-
-            // Process the result set and extract relevant information
-            List<Map<String, String>> resultList = new ArrayList<>();
-            while (results.hasNext()) {
-                // Extract variables from each result row and add them to the resultList
-                QuerySolution solution = results.nextSolution();
-                Map<String, String> resultMap = new HashMap<>();
-
-                // Extract itemLabel
-                RDFNode itemLabelNode = solution.get("itemLabel");
-                if (itemLabelNode != null) {
-                    resultMap.put("itemLabel", itemLabelNode.toString());
-                }
-
-                // Extract where
-                RDFNode whereNode = solution.get("where");
-                if (whereNode != null) {
-                    resultMap.put("where", whereNode.toString());
-                }
-
-                // Extract url
-                RDFNode urlNode = solution.get("url");
-                if (urlNode != null) {
-                    resultMap.put("url", urlNode.toString());
-                }
-                resultList.add(resultMap);
-            }
-            return resultList;
-        } finally {
-            // Release resources
-            qexec.close();
+            return processQueryResults(results);
         }
+    }
+
+    private List<Map<String, String>> processQueryResults(ResultSet results) {
+        List<Map<String, String>> resultList = new ArrayList<>();
+        while (results.hasNext()) {
+            QuerySolution solution = results.nextSolution();
+            Map<String, String> resultMap = new HashMap<>();
+            RDFNode itemLabelNode = solution.get("itemLabel");
+            if (itemLabelNode != null) {
+                resultMap.put("itemLabel", itemLabelNode.toString());
+            }
+            RDFNode whereNode = solution.get("where");
+            if (whereNode != null) {
+                resultMap.put("where", whereNode.toString());
+            }
+            RDFNode urlNode = solution.get("url");
+            if (urlNode != null) {
+                resultMap.put("url", urlNode.toString());
+            }
+            resultList.add(resultMap);
+        }
+        return resultList;
     }
 
 
     private String constructSparqlQuery(Double lat1, Double lon1, Double lat2, Double lon2) {
-        String sparqlQuery =
-                        "PREFIX schema: <http://schema.org/>\n" +
-                        "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
-                        "PREFIX geo: <http://www.opengis.net/ont/geosparql#>\n" +
-                        "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
-                        "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
-                        "\n" +
-                        "SELECT DISTINCT ?item ?itemLabel ?when ?where ?url\n" +
-                        "WHERE {\n" +
-                        "  ?url schema:about ?item .\n" +
-                        "  ?url schema:inLanguage \"pt\" .\n" +
-                        "  FILTER (STRSTARTS(str(?url), \"https://pt.wikipedia.org/\")) .\n" +
-                        "  SERVICE wikibase:box {\n" +
-                        "    ?item wdt:P625 ?where .\n" +
-                        "    bd:serviceParam wikibase:cornerSouthWest \"Point(-9.52 36.95)\"^^geo:wktLiteral .\n" +
-                        "    bd:serviceParam wikibase:cornerNorthEast \"Point(-6.18 42.16)\"^^geo:wktLiteral .\n" +
-                        "  }\n" +
-                        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE]\" . }\n" +
-                        "}\n" +
-                        "ORDER BY ASC(?when)\n" +
-                        "LIMIT 1000";
         // Return the SPARQL query
-        return sparqlQuery;
+        return "PREFIX schema: <http://schema.org/>\n" +
+        "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+        "PREFIX geo: <http://www.opengis.net/ont/geosparql#>\n" +
+        "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
+        "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+        "\n" +
+        "SELECT DISTINCT ?item ?itemLabel ?when ?where ?url\n" +
+        "WHERE {\n" +
+        "  ?url schema:about ?item .\n" +
+        "  ?url schema:inLanguage \"pt\" .\n" +
+        "  FILTER (STRSTARTS(str(?url), \"https://pt.wikipedia.org/\")) .\n" +
+        "  SERVICE wikibase:box {\n" +
+        "    ?item wdt:P625 ?where .\n" +
+        "    bd:serviceParam wikibase:cornerSouthWest \"Point(-9.52 36.95)\"^^geo:wktLiteral .\n" +
+        "    bd:serviceParam wikibase:cornerNorthEast \"Point(-6.18 42.16)\"^^geo:wktLiteral .\n" +
+        "  }\n" +
+        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE]\" . }\n" +
+        "}\n" +
+        "ORDER BY ASC(?when)\n" +
+        "LIMIT 1000";
     }
     private String constructSparqlQueryTime(Double lat1, Double lon1, Double lat2, Double lon2, Long startTime, Long endTime) {
         // Construct the SPARQL query with variables
-        String sparqlQuery = "PREFIX schema: <http://schema.org/>\n" +
+
+        // Return the SPARQL query
+        return "PREFIX schema: <http://schema.org/>\n" +
                 "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
                 "PREFIX geo: <http://www.opengis.net/ont/geosparql#>\n" +
                 "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
@@ -134,9 +137,15 @@ public class SearchService {
                 "ORDER BY ASC(?when)\n" +
                 "LIMIT 1000\n" +
                 "";
-
-        // Return the SPARQL query
-        return sparqlQuery;
+    }
+    private void validateCoordinates(Double lat1, Double lon1, Double lat2, Double lon2) {
+        if (lat1 == null || lon1 == null || lat2 == null || lon2 == null) {
+            throw new IllegalArgumentException("Latitude and longitude values cannot be null");
+        }
+        if (lat1 < -90 || lat1 > 90 || lon1 < -180 || lon1 > 180 ||
+                lat2 < -90 || lat2 > 90 || lon2 < -180 || lon2 > 180) {
+            throw new IllegalArgumentException("Invalid latitude or longitude values");
+        }
     }
 
 
