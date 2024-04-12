c9aca1faa04b531f907c807d59c4c5c91efa690a
Tiago Pereira <tpereira1911@gmail.com>
2024-04-12 15:50:59 +0100

    GetUsers/GetUsersById/GetUsersByRole - EndPoints Changed and Error Handling

--- src/main/java/projeto/projetoinformatico/controller/UserController.java
+++ src/main/java/projeto/projetoinformatico/controller/UserController.java
@@ -4,10 +4,13 @@ package projeto.projetoinformatico.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import projeto.projetoinformatico.service.UserService;
+import projeto.projetoinformatico.users.Role;
 import projeto.projetoinformatico.users.User;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api") ///////////////<-------------
+@RequestMapping("/api")
 public class UserController {
     private final UserService userService;
 
@@ -23,9 +26,28 @@ public class UserController {
         return ResponseEntity.ok(newUser);
     }
 
-    @GetMapping("/{username}")
+    @GetMapping("/users/{username}")
     public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
         User user = userService.getUserByUsername(username);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+    @GetMapping("/users/role/{role}")
+    public ResponseEntity<List<User>> getAllUsersByRole(@PathVariable Role role) {
+
+        List<User> users = userService.getAllUsersByRole(role);
+        return ResponseEntity.ok(users);
+    }
+    @GetMapping("/users/id/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
--- src/main/java/projeto/projetoinformatico/service/UserService.java
+++ src/main/java/projeto/projetoinformatico/service/UserService.java
@@ -1,12 +1,18 @@
 package projeto.projetoinformatico.service;
 
+import org.apache.jena.shared.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import projeto.projetoinformatico.users.Role;
 import projeto.projetoinformatico.users.User;
 import projeto.projetoinformatico.users.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -32,8 +38,28 @@ public class UserService{
         return userRepository.save(user);
     }
 
+    @Cacheable(value = "searchCache", key="{#username}")
     public User getUserByUsername(String username) {
-        return userRepository.findByUsername(username);
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return user;
     }
 
+    @Cacheable(value = "searchCache")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Cacheable(value = "searchCache", key = "{ #role}")
+    public List<User> getAllUsersByRole(Role role) {
+        return userRepository.findAllByRole(role);
+    }
+
+    @Cacheable(value = "searchCache", key = "{ #id }")
+    public User getUserById(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+    }
 }
