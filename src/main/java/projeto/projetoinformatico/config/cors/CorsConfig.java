e3d4eb30c285954b8b897da963cfba0fe299bd6a
Tiago Pereira <tpereira1911@gmail.com>
2024-04-21 17:49:07 +0100

    Cors

--- src/main/java/projeto/projetoinformatico/config/cors/CorsConfig.java
+++ src/main/java/projeto/projetoinformatico/config/cors/CorsConfig.java
@@ -1,2 +1,16 @@
-package projeto.projetoinformatico.config.cors;public class CorsConfig {
+package projeto.projetoinformatico.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
 }
