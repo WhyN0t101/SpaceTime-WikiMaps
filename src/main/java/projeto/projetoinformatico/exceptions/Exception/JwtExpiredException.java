1f6417927aeb5c1c84c3a743c46b9db8cc1b95cc
Diogo Andrade <psvdiogo@gmail.com>
2024-04-30 10:32:56 +0100

    Notes

--- src/main/java/projeto/projetoinformatico/exceptions/Exception/JwtExpiredException.java
+++ src/main/java/projeto/projetoinformatico/exceptions/Exception/JwtExpiredException.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
-public class JwtExpiredException extends RuntimeException{
+public class JwtExpiredException extends RuntimeException{//alterar
     public JwtExpiredException(String message) {
         super(message);
     }
