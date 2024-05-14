f215e65976e9f918759e01757eeed9ecf1616c50
Diogo Andrade <psvdiogo@gmail.com>
2024-05-14 20:38:06 +0100

    AlterPasswordUpdated

--- src/main/java/projeto/projetoinformatico/controllers/PasswordController.java
+++ src/main/java/projeto/projetoinformatico/controllers/PasswordController.java
@@ -3,6 +3,7 @@ package projeto.projetoinformatico.controllers;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import projeto.projetoinformatico.dtos.UserDTO;
@@ -26,10 +27,12 @@ public class PasswordController {
     }
 
 
-    @PutMapping("/users/{userId}/password")
+    @PutMapping("/users/password")
     @PreAuthorize("hasAuthority('EDITOR') or hasAuthority('ADMIN') or hasAuthority('USER')")
-    public ResponseEntity<UserDTO> updatePassword(@PathVariable Long userId, @Valid @RequestBody UpdatePasswordRequest updatePasswordRequest) {
-        UserDTO user = userService.getUserById(userId);
+    public ResponseEntity<UserDTO> updatePassword(@Valid @RequestBody UpdatePasswordRequest updatePasswordRequest,
+                                                  Authentication authentication) {
+        String username = authentication.getName();
+        UserDTO user = userService.getUserByUsername(username);
         if(!passwordService.validatePassword(user,updatePasswordRequest.getOldPassword())){
             throw new InvalidParamsRequestException("Old password does not match");
         }
